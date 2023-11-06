101afc8ef4da765dddaddd30e67a4b50c77f570b
yunussezgin <yunussezgin8@hotmail.com>
2023-11-06 22:03:22 +0100

    Add parallel run task

---
+++ src/main/java/org/example/thread/Parallelism.java
@@ -0,0 +1,33 @@
+package org.example.thread;
+
+class Task extends Thread {
+
+    // Thread execution begins here.
+    public void run() {
+        // performs the task i.e. prints 1500 T's
+        doTask();
+    }
+
+    public void doTask() {
+        for (int i = 1; i <= 1500; i++) {
+            System.out.print("S");
+        }
+    }
+}
+
+public class Parallelism {
+
+    // Runs with in the Main thread started by JVM.
+    public static void main(String[] args) {
+
+        Task t1 = new Task();
+        // Starts a separate Thread using the
+        // the start method of the Thread class.
+        t1.start();
+
+        // runs in the Main thread and prints 1500 M's
+        for (int i = 1; i <= 1500; i++) {
+            System.out.print("Y");
+        }
+    }
+}
